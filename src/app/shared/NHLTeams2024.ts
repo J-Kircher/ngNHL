635bb32013a4837e3389c492ddcc5b717b4101a3
Jim <James.C.KIrcher@gmail.com>
2024-10-03 13:39:45 -0400

    Add Utah Hockey Club

--- src/app/shared/NHLTeams2024.ts
+++ src/app/shared/NHLTeams2024.ts
@@ -17,13 +17,13 @@ export const _TEAMS: ITeam[] = [
   { 'city': 'Ottawa',       'name': 'Senators',       'abbrev': 'OTT', 'lightcolor': 'DE8C00', 'darkcolor': 'DE2129', 'chartcolor': '989898', 'division': 'East Atlantic', 'of': 7, 'de': 5, 'pp': 6, 'pk': 5, 'go': 5, 'co': 7 },
   { 'city': 'Tampa Bay',    'name': 'Lightning',      'abbrev': 'TB',  'lightcolor': 'FFFFFF', 'darkcolor': '04236C', 'chartcolor': '06D1FF', 'division': 'East Atlantic', 'of': 9, 'de': 6, 'pp': 9, 'pk': 8, 'go': 6, 'co': 7 },
   { 'city': 'Toronto',      'name': 'Maple Leafs',    'abbrev': 'TOR', 'lightcolor': 'FFFFFF', 'darkcolor': '002462', 'chartcolor': '002462', 'division': 'East Atlantic', 'of': 9, 'de': 6, 'pp': 8, 'pk': 6, 'go': 6, 'co': 7 },
-  { 'city': 'Arizona',      'name': 'Coyotes',        'abbrev': 'ARI', 'lightcolor': 'FFD6AD', 'darkcolor': '840021', 'chartcolor': '840021', 'division': 'West Central',  'of': 8, 'de': 6, 'pp': 7, 'pk': 6, 'go': 6, 'co': 7 },
   { 'city': 'Chicago',      'name': 'Blackhawks',     'abbrev': 'CHI', 'lightcolor': 'FF0018', 'darkcolor': '000000', 'chartcolor': 'FF0018', 'division': 'West Central',  'of': 5, 'de': 5, 'pp': 6, 'pk': 5, 'go': 5, 'co': 7 },
   { 'city': 'Colorado',     'name': 'Avalanche',      'abbrev': 'COL', 'lightcolor': 'ADAEF7', 'darkcolor': '730021', 'chartcolor': 'ADAEF7', 'division': 'West Central',  'of': 9, 'de': 7, 'pp': 8, 'pk': 7, 'go': 7, 'co': 7 },
   { 'city': 'Dallas',       'name': 'Stars',          'abbrev': 'DAL', 'lightcolor': 'A5AAAD', 'darkcolor': '0F714A', 'chartcolor': '427D65', 'division': 'West Central',  'of': 9, 'de': 8, 'pp': 8, 'pk': 8, 'go': 8, 'co': 7 },
   { 'city': 'Minnesota',    'name': 'Wild',           'abbrev': 'MIN', 'lightcolor': 'FFDE08', 'darkcolor': '085239', 'chartcolor': '085239', 'division': 'West Central',  'of': 7, 'de': 6, 'pp': 7, 'pk': 5, 'go': 7, 'co': 7 },
   { 'city': 'Nashville',    'name': 'Predators',      'abbrev': 'NAS', 'lightcolor': 'FDB801', 'darkcolor': '001D43', 'chartcolor': 'FDB801', 'division': 'West Central',  'of': 8, 'de': 7, 'pp': 7, 'pk': 6, 'go': 7, 'co': 7 },
   { 'city': 'St. Louis',    'name': 'Blues',          'abbrev': 'STL', 'lightcolor': 'FFE700', 'darkcolor': '004273', 'chartcolor': '004273', 'division': 'West Central',  'of': 7, 'de': 7, 'pp': 6, 'pk': 7, 'go': 7, 'co': 7 },
+  { 'city': 'Utah',         'name': 'Hockey Club',    'abbrev': 'UTH', 'lightcolor': '3A6182', 'darkcolor': '000000', 'chartcolor': '3A6182', 'division': 'West Central',  'of': 8, 'de': 6, 'pp': 7, 'pk': 6, 'go': 6, 'co': 7 },
   { 'city': 'Winnipeg',     'name': 'Jets',           'abbrev': 'WPG', 'lightcolor': 'C41234', 'darkcolor': '042E64', 'chartcolor': 'C41234', 'division': 'West Central',  'of': 8, 'de': 9, 'pp': 6, 'pk': 6, 'go': 9, 'co': 7 },
   { 'city': 'Anaheim',      'name': 'Ducks',          'abbrev': 'ANA', 'lightcolor': 'B59463', 'darkcolor': '212121', 'chartcolor': 'B59463', 'division': 'West Pacific',  'of': 6, 'de': 5, 'pp': 6, 'pk': 5, 'go': 5, 'co': 7 },
   { 'city': 'Calgary',      'name': 'Flames',         'abbrev': 'CGY', 'lightcolor': 'FFEF39', 'darkcolor': 'DE1018', 'chartcolor': 'FFEF39', 'division': 'West Pacific',  'of': 8, 'de': 6, 'pp': 6, 'pk': 8, 'go': 6, 'co': 7 },
@@ -35,6 +35,6 @@ export const _TEAMS: ITeam[] = [
   { 'city': 'Vegas',        'name': 'Golden Knights', 'abbrev': 'LV',  'lightcolor': '8E7349', 'darkcolor': '344248', 'chartcolor': '8E7349', 'division': 'West Pacific',  'of': 8, 'de': 7, 'pp': 7, 'pk': 7, 'go': 7, 'co': 7 }
 ];
 
-// Copied from NHLTeams2022 on Thu Jul 13, 2023
-// Modified on Wed Apr 24, 2024
-// end of NHLTeams2023
+// Copied from NHLTeams2023 on Thu Oct 3, 2024
+// Modified on Thu Oct 3, 2024
+// end of NHLTeams2024
